ff05e293486d75ec594699a430fb7603ee676c0b
islamnashaat03 <islamnashaat03@gmail.com>
2021-03-15 01:27:37 +0200

    2

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-orange',
+      radioClass: 'iradio_square-orange',
+      increaseArea: '20%' // optional
+    });
+  });
